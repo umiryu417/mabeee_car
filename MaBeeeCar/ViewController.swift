f8aadadfd447299f4e22d0ed35b15de0846299bc
増田崇 <umiryu417@gmail.com>
2016-09-18 23:09:07 +0900

    Initial Commit

---
+++ MaBeeeCar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaBeeeCar
+//
+//  Created by 増田崇 on 2016/09/18.
+//  Copyright © 2016年 umiryu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
